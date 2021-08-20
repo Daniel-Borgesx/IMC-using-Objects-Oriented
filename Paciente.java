0c1c1f7ecc4723bdc5cca48d4396c2944de83e9c
Daniel-Borgesx <87291287+Daniel-Borgesx@users.noreply.github.com>
2021-08-20 15:21:32 -0300

    Add files via upload

--- Paciente.java
+++ Paciente.java
@@ -9,19 +9,13 @@ public class Paciente {
 	double calculoDeMassaCorporal() {
 		IMC imc = new IMC();
 		
-		double imcPaciente = peso / (altura * altura);
-		System.out.println("O IMC do paciente " + nome + " é de: " + imcPaciente);		
-		if (imcPaciente <= 18.5) {
-			System.out.println(imc.abaixoDoPeso);
-		} else if (imcPaciente <= 25.5) {
-			System.out.println(imc.pesoIdeal);
-		} else if (imcPaciente <= 30.0) {
-			System.out.println(imc.acimaDoPeso);
-		} else if (imcPaciente >= 30.1) {
-			System.out.println(imc.pacienteObeso);
-		}
+		imc.imcPaciente = peso / (altura * altura);
+		
+		System.out.println("O IMC do paciente " + nome + " é de: " + imc.imcPaciente);		
+		
+		imc.resultadoDoIMCDoPaciente();
 	    
-		return imcPaciente;
+		return imc.imcPaciente;
 	}
 
 }
